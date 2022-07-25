2395536a1b3640339e0d6e428d7b3f283efa1f03
Lem Euro <mr.lemeuro@gmail.com>
2022-07-25 12:38:35 +0300

    Initial Commit

---
+++ Sexism/SexismApp.swift
@@ -0,0 +1,17 @@
+//
+//  SexismApp.swift
+//  Sexism
+//
+//  Created by Lem Euro on 25.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct SexismApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
